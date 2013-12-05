86daf00fd519a1a272ae34206a3167dd19a660fa
Przemysław Walkowiak <przemyslaw.walkowiak@put.poznan.pl>
2013-12-05 18:38:26 +0100

    added option to install only one package

--- dotfiles.py
+++ dotfiles.py
@@ -7,6 +7,7 @@ Created by Przemyslaw Walkowiak <przemkovv@gmail.com>
 
 Usage:
     dotfiles.py install [DOTFILES_PATH] [PATH]
+    dotfiles.py install_package <package> [DOTFILES_PATH] [PATH]
     dotfiles.py update [DOTFILES_PATH]
 
 Arguments:
@@ -27,27 +28,38 @@ def list_packages(path):
     for entry in os.listdir(path):
         entry_path = os.path.join(path, entry)
         if os.path.isdir(entry_path) and not entry.startswith("."):
-            yield entry_path
+            yield entry
 
 
 def make_symlink(source, target):
-    print "%s -> %s" % (source, target)
+    print "\t%s -> %s" % (source, target)
     if os.path.exists(target):
         os.remove(target)
     os.symlink(source, target)
 
 
-def install_package(package_path, dst_path):
+def get_package_path(package, dotfiles_path):
+    package_path = os.path.join(dotfiles_path, package)
+    if not os.path.exists(package_path):
+        raise IOError("Package '%s' cannot be found. Path '%s' doesn't exists."
+                      % (package, package_path))
+    return package_path
+
+
+def install_package(package, dotfiles_path, dst_path):
+    print("Installing package '%s':" % package)
+    package_path = get_package_path(package, dotfiles_path)
     for entry in os.listdir(package_path):
-        if not entry.startswith(".git"):
-            source = os.path.join(package_path, entry)
-            target = os.path.join(dst_path, ".%s" % entry)
-            make_symlink(source, target)
+        if entry.startswith(".git"):
+            continue
+        source = os.path.join(package_path, entry)
+        target = os.path.join(dst_path, ".%s" % entry)
+        make_symlink(source, target)
 
 
 def install(dotfiles_path, dst_path):
     for package in list_packages(dotfiles_path):
-        install_package(package, dst_path)
+        install_package(package, dotfiles_path, dst_path)
     #for package in os.listdir(dotfiles):
         #pass
 
@@ -57,25 +69,34 @@ def update(dotfiles_path):
 
 
 def main(args):
-    DEFAULT_PATH = "~"
-    DEFAULT_DOTFILES_PATH = "~/dotfiles"
 
-    dst_path = args["PATH"] or DEFAULT_PATH
-    dotfiles_path = args["DOTFILES_PATH"] or DEFAULT_DOTFILES_PATH
+    try:
+        DEFAULT_PATH = "~"
+        DEFAULT_DOTFILES_PATH = "~/dotfiles"
 
-    dst_path = os.path.expanduser(dst_path)
-    dotfiles_path = os.path.expanduser(dotfiles_path)
+        dst_path = args["PATH"] or DEFAULT_PATH
+        dotfiles_path = args["DOTFILES_PATH"] or DEFAULT_DOTFILES_PATH
 
-    if not os.path.exists(dst_path):
-        raise IOError("Destination path '%s' cannot be found" % dst_path)
-    if not os.path.exists(dotfiles_path):
-        raise IOError("Dotfiles path '%s' cannot be found" % dotfiles_path)
+        dst_path = os.path.expanduser(dst_path)
+        dotfiles_path = os.path.expanduser(dotfiles_path)
+        dotfiles_path = os.path.abspath(dotfiles_path)
 
-    if args['install']:
-        install(dotfiles_path, dst_path)
-    elif args['update']:
-        update(dotfiles_path)
+        if not os.path.exists(dst_path):
+            raise IOError("Destination path '%s' cannot be found." % dst_path)
+        if not os.path.exists(dotfiles_path):
+            raise IOError("Dotfiles path '%s' cannot be found."
+                          % dotfiles_path)
 
+        if args['install']:
+            install(dotfiles_path, dst_path)
+        if args['install_package']:
+            package_name = args["<package>"]
+            install_package(package_name, dotfiles_path, dst_path)
+        elif args['update']:
+            update(dotfiles_path)
+
+    except IOError as ex:
+        print "I/O error: %s" % ex
 
 if __name__ == '__main__':
     main(docopt(__doc__, version='0.1alpha'))
