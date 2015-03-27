2016b84b530322b2afb15749a8a28c347f6d81fa
Przemysław Walkowiak <przemyslaw.walkowiak@put.poznan.pl>
2015-03-27 13:47:43 +0100

    merged

--- i3/i3/status.py
+++ i3/i3/status.py
@@ -21,8 +21,8 @@ status.register("cpu_usage",
                 format="↺ {usage:02}%")
 
 # Shows your CPU temperature, if you have a Intel CPU
-status.register("temp",
-                format="{temp:.0f}°C",)
+#status.register("temp",
+                #format="{temp:.0f}°C",)
 
 # This would look like this, when discharging (or charging)
 # ↓14.22W 56.15% [77.81%] 2h:41m
@@ -31,15 +31,15 @@ status.register("temp",
 #
 # This would also display a desktop notification (via dbus) if the percentage
 # goes below 5 percent while discharging. The block will also color RED.
-status.register("battery",
-    format="{status}{consumption:.2f}W {percentage:.2f}% \[{percentage_design:.2f}%\] {remaining:%E%hh:%Mm}",
-    alert=True,
-    alert_percentage=5,
-    status={
-        "DIS": "↓",
-        "CHR": "↑",
-        "FULL": "=",
-    },)
+#status.register("battery",
+    #format="{status}{consumption:.2f}W {percentage:.2f}% \[{percentage_design:.2f}%\] {remaining:%E%hh:%Mm}",
+    #alert=True,
+    #alert_percentage=5,
+    #status={
+        #"DIS": "↓",
+        #"CHR": "↑",
+        #"FULL": "=",
+    #},)
 
 # Has all the options of the normal network and adds some wireless specific things
 # like quality and network names.
@@ -63,7 +63,7 @@ status.register("battery",
 #
 # Note: the network module requires PyPI package netifaces-py3
 status.register("network",
-                interface="wlp3s0",
+                interface="wlp6s0",
                 dynamic_color=False,
                 format_up="{interface}: {v4cidr}, {essid} {quality:03.0f}%, ↓{bytes_recv} KB/s ↑{bytes_sent} KB/s",)
 
