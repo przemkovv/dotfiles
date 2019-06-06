10633f4b5ea6548b04e63d5ad9d1ce5f675cbb7c
Przemysław Walkowiak <przemyslaw.walkowiak@put.poznan.pl>
2019-06-06 12:49:10 +0200

    Polybar for gandalf.

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -7,6 +7,14 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
-polybar legolas &
+if [ "$HOSTNAME" = "legolas" ]; then
+  polybar legolas &
+elif [ "$HOSTNAME" = "gandalf" ]; then
+  polybar gandalf_dvi1 &
+  polybar gandalf_dp0 &
+  polybar gandalf_hdmi0 &
+else
+  polybar legolas &
+fi
 
 echo "Polybar launched..."
